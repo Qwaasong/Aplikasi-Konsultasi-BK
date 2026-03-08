1522cb8260a17bafef44fe0439c92c64432717fc
Ray-Si-Arga <argaa3093@gmail.com>
2026-03-08 11:20:28 +0700

    Penambahan beberapa components/images, logo SMK dan judul serta deskripsi serta folder images

---
+++ resources/views/components/judul.blade.php
@@ -0,0 +1,4 @@
+<h1 {{ $attributes }}>
+    {{ $slot }}
+</h1>
+
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,55 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex">
+
+        <!-- KIRI : FORM -->
+        <div class="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-100 space-y-6">
+
+            <div class="w-full sm:max-w-md space-y-6">
+
+                <div class="mb-12">
+                    <x-judul class="text-3xl font-bold text-left">
+                        {{ __('Welcome Back') }}
+                    </x-judul>
+
+                    <x-deskripsi class="text-gray-600 mt-2 mb-8">
+                        Bukan Member?
+                        <a href="#" class="text-[var(--primary-color)] hover:underline">Daftar Disini</a>
+                    </x-deskripsi>
+                </div>
+
+
+                <div class="px-6 py-4 shadow-md sm:rounded-lg bg-white">
+                    {{ $slot }}
+                </div>
+
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
         </div>
-    </body>
+
+        <!-- KANAN : GAMBAR -->
+        <div class="hidden md:block md:w-1/2">
+            <img src="{{ asset('images/wallpaper.jpg') }}" class="h-screen w-full object-cover">
+        </div>
+
+    </div>
+</body>
+
 </html>
