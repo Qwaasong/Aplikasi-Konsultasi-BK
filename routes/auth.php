a99b4c67a8d37ca01dd0659f2f62d61bbcdb6793
hakuuu <ahaqqurrahman12@gmail.com>
2026-04-29 10:06:48 +0700

    fix error pada auth

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 use App\Livewire\Actions\Logout;
 
+// 1. Logic halaman depan
 Route::get('/', function () {
     if (auth()->check()) {
         if (auth()->user()->role == 'admin') {
@@ -14,6 +15,16 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
+// 2. Route untuk yang BELUM login (Ini yang tadi hilang)
+Route::middleware('guest')->group(function () {
+    Volt::route('register', 'pages.auth.register')
+        ->name('register');
+
+    Volt::route('login', 'pages.auth.login')
+        ->name('login'); // Nama ini yang dicari oleh sistem
+});
+
+// 3. Route untuk yang SUDAH login
 Route::middleware('auth')->group(function () {
     Route::get('logout', function (Logout $logout) {
         $logout();
