fe71961926d8a322ac175b58901da7c4fb1424f5
Vira <viraaulia244@gmail.com>
2026-07-09 10:33:01 +0700

    Menyesuaikan tampilan halaman konselor dengan template konsultasi

--- resources/views/livewire/pages/konselor/peminatan/index.blade.php
+++ resources/views/livewire/pages/konselor/peminatan/index.blade.php
@@ -5,20 +5,79 @@ use Livewire\Attributes\Layout;
 
 new #[Layout('layouts.app')] class extends Component {
 
+    public string $search = '';
+    public bool $selectAll = false;
+
+    // Placeholder sementara
+    public array $records = [];
+
 };
 
 ?>
 
-<div class="py-8">
-    <div class="mx-auto sm:px-6 lg:px-8">
+<div class="flex-1 flex flex-col min-w-0 bg-white h-full">
 
-        <h1 class="text-3xl font-bold text-brand-teal">
-            Peminatan Siswa
-        </h1>
+    {{-- Header --}}
+    <x-organisms.header>
+        <x-slot:search>
+            <x-molecules.search-input model="search" />
+        </x-slot:search>
 
-        <p class="mt-2 text-gray-500">
-            Halaman ini masih dalam tahap pengembangan.
-        </p>
+        Peminatan
+    </x-organisms.header>
 
+    {{-- Toolbar --}}
+    <x-organisms.table-toolbar>
+        <x-slot:pagination>
+            {{ count($records) }} data
+        </x-slot:pagination>
+    </x-organisms.table-toolbar>
+
+    {{-- Flash Message --}}
+    <div class="px-4 py-2">
+        <x-shared.flash-message />
     </div>
+
+    {{-- Data Table --}}
+    <x-organisms.data-table empty="Belum ada data peminatan.">
+
+        @forelse($records as $record)
+
+            <tr
+                class="group border-b border-gray-100 transition-all duration-200 h-12 relative cursor-pointer hover:shadow-[0_2px_10px_-3px_rgba(0,0,0,0.1),0_4px_6px_-4px_rgba(0,0,0,0.1)] hover:z-10 hover:rounded-md bg-white">
+
+                <td class="w-16 text-center align-middle py-2">
+                    <input
+                        type="checkbox"
+                        class="w-4 h-4 rounded border-gray-300 accent-brand-teal">
+                </td>
+
+                <td class="px-4 py-2 font-semibold">
+                    {{ $record['nama'] }}
+                </td>
+
+                <td class="px-4 py-2 text-sm text-gray-600">
+                    {{ $record['kelas'] }}
+                </td>
+
+                <td class="px-4 py-2 text-sm text-gray-600">
+                    {{ $record['orang_tua'] }}
+                </td>
+
+                <td class="px-4 py-2 text-sm text-gray-600">
+                    {{ $record['tanggal'] }}
+                </td>
+
+                <td class="px-4 py-2 text-right">
+                    -
+                </td>
+
+            </tr>
+
+        @empty
+
+        @endforelse
+
+    </x-organisms.data-table>
+
 </div>
