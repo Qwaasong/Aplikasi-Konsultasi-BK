e7b28a95e7c5f9fe3b607ef6b630008221ef2f7c
Nusvara <yusufusman11111111@gmail.com>
2026-08-03 11:10:26 +0700

    jfhyjfuj

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             FirstDatabaseSeeder::class,
-            OAuthClientSeeder::class,
             DataSiswaSeeder::class,
             KonsultasiSeeder::class,
             // FactorySeeder::class,
