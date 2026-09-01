f921d2d7910ee3cf623ec2115e046c58533538e6
nasywa <nanaschwan20@gmail.com>
2026-09-01 07:32:38 +0700

    mengganti title di landing page

--- resources/views/landing/sections/navbar.blade.php
+++ resources/views/landing/sections/navbar.blade.php
@@ -12,7 +12,7 @@
             <div class="navbar-brand-text">
 
                 <span class="navbar-brand-title">
-                    Aplikasi Konsultasi BK
+                    BK Skanawa
                 </span>
 
                 <span class="navbar-brand-subtitle">
