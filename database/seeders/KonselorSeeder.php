f0b059675c4e02c1ba0f6f6cadea5ac289a3f75e
Nusvara <yusufusman11111111@gmail.com>
2026-09-15 11:18:32 +0700

    fix: memperbaiki seeder

--- database/seeders/KonselorSeeder.php
+++ database/seeders/KonselorSeeder.php
@@ -22,7 +22,7 @@ class KonselorSeeder extends Seeder
                 'email'         => 'danny@sekolah.sch.id',
                 'jenis_kelamin' => 'L',
                 'no_hp'         => '081200000001',
-                'nip'           => 'GBK' . date('Ymd') . '01',
+                'nip'           => 'GBK001',
                 'jabatan'       => 'Guru BK',
             ],
             [
@@ -31,7 +31,7 @@ class KonselorSeeder extends Seeder
                 'email'         => 'bening@sekolah.sch.id',
                 'jenis_kelamin' => 'P',
                 'no_hp'         => '081200000002',
-                'nip'           => 'GBK' . date('Ymd') . '02',
+                'nip'           => 'GBK002',
                 'jabatan'       => 'Guru BK',
             ],
             [
@@ -40,7 +40,7 @@ class KonselorSeeder extends Seeder
                 'email'         => 'mayang@sekolah.sch.id',
                 'jenis_kelamin' => 'P',
                 'no_hp'         => '081200000003',
-                'nip'           => 'GBK' . date('Ymd') . '03',
+                'nip'           => 'GBK003',
                 'jabatan'       => 'Guru BK',
             ],
             [
@@ -49,12 +49,38 @@ class KonselorSeeder extends Seeder
                 'email'         => 'bawon@sekolah.sch.id',
                 'jenis_kelamin' => 'P',
                 'no_hp'         => '081200000004',
-                'nip'           => 'GBK' . date('Ymd') . '04',
+                'nip'           => 'GBK004',
                 'jabatan'       => 'Guru BK',
             ],
         ];
 
-        $this->command->info('✅ 4 konselor berhasil di-seed: Pak Danny, Bu Bening, Bu Mayang, Bu Bawon');
+        foreach ($konselor as $data) {
+            $user = User::updateOrCreate(
+                ['email' => $data['email']],
+                [
+                    'nama' => $data['nama'],
+                    'username' => $data['username'],
+                    'no_hp' => $data['no_hp'],
+                    'jenis_kelamin' => $data['jenis_kelamin'],
+                    'foto' => '',
+                    'status' => 'aktif',
+                    'role' => 'guru_bk',
+                    'password' => Hash::make('password123'),
+                ]
+            );
+
+            Pegawai::updateOrCreate(
+                ['user_id' => $user->id],
+                [
+                    'nip' => $data['nip'],
+                    'jabatan' => $data['jabatan'],
+                ]
+            );
+
+            $this->command->info("Konselor {$data['nama']} siap login dengan username: {$data['username']}");
+        }
+
+        $this->command->info('4 konselor berhasil di-seed: Dani, Bening, Mayang, Bawon');
         $this->command->info('   Password default: password123');
     }
 }
